ddc224af75ca531d408afbcc03c87970f59dbf5b
danielfersanchez <38057662+danielfersanchez@users.noreply.github.com>
2018-06-06 18:33:20 -0500

    push

--- DepServices/DepServices/MainPage.xaml.cs
+++ DepServices/DepServices/MainPage.xaml.cs
@@ -13,9 +13,22 @@ namespace DepServices
         {
             InitializeComponent();
             this.btnMessage.Clicked += BtnMessage_Clicked;
+            this.btnPlataforma.Clicked += BtnPlataforma_Clicked;
 
         }
 
+        private async void BtnPlataforma_Clicked(object sender, EventArgs e)
+        {
+            await Task.Delay(5000).ConfigureAwait(false);
+            Device.StartTimer(TimeSpan.FromSeconds(1), () =>
+             {
+                 Device.BeginInvokeOnMainThread(() =>
+                 this.txtAviso.Text = DateTime.Now.ToString());
+                 return true;
+             }
+
+            );
+        }
 
         private async void BtnMessage_Clicked(object sender, EventArgs e)
         {
